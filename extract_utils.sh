89d4754d8f6e71103380492df34dd425234fe3c9
slogar25 <michaelslogar@gmail.com>
2024-10-12 14:49:03 +0000

    extract-utils: extract vendor_dlkm out of OTA and super - Manually picked from Lineage Gerrit for komodo - Thanks to Cosmin Tanislav
    Change-Id: I33a383af1b4945dc670f0ac249f550b8c1067b7c

--- extract_utils.sh
+++ extract_utils.sh
@@ -1892,14 +1892,14 @@ function prepare_images() {
 
             # Extract A/B OTA
             if [ -a "$DUMPDIR"/payload.bin ]; then
-                for PARTITION in "system" "odm" "product" "system_ext" "vendor"; do
+                for PARTITION in "system" "odm" "product" "system_ext" "vendor" "vendor_dlkm"; do
                     "$OTA_EXTRACTOR" --payload "$DUMPDIR"/payload.bin --output_dir "$DUMPDIR" --partitions "$PARTITION" &
                     2>&1
                 done
                 wait
             fi
 
-            for PARTITION in "system" "odm" "product" "system_ext" "vendor"; do
+            for PARTITION in "system" "odm" "product" "system_ext" "vendor" "vendor_dlkm"; do
                 # If OTA is block based, extract it.
                 if [ -a "$DUMPDIR"/"$PARTITION".new.dat.br ]; then
                     echo "Converting $PARTITION.new.dat.br to $PARTITION.new.dat"
@@ -1937,7 +1937,7 @@ function prepare_images() {
         echo "Unpacking super.img"
         "$SIMG2IMG" "${SUPERIMGS[@]}" "$DUMPDIR"/super.raw
 
-        for PARTITION in "system" "odm" "product" "system_ext" "vendor"; do
+        for PARTITION in "system" "odm" "product" "system_ext" "vendor" "vendor_dlkm"; do
             echo "Preparing $PARTITION"
             if "$LPUNPACK" -p "$PARTITION"_a "$DUMPDIR"/super.raw "$DUMPDIR"; then
                 mv "$DUMPDIR"/"$PARTITION"_a.img "$DUMPDIR"/"$PARTITION".img
@@ -1959,7 +1959,7 @@ function prepare_images() {
         DUMPDIR="$EXTRACT_TMP_DIR"/system_dump
         mkdir -p "$DUMPDIR"
 
-        for PARTITION in "system" "odm" "product" "system_ext" "vendor"; do
+        for PARTITION in "system" "odm" "product" "system_ext" "vendor" "vendor_dlkm"; do
             echo "Extracting $PARTITION"
             local IMAGE="$SRC"/"$PARTITION".img
             if [ -f "$IMAGE" ]; then
