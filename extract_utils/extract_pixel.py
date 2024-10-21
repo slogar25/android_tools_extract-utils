00f83979b33a46ed9638f20c42850f138bba647a
Cosmin Tanislav <demonsingur@gmail.com>
2024-10-21 19:59:20 +0300

    extract_utils: add extract function for extracting pixel factory images
    Change-Id: Icf92b51d69c82699e685bd5131a35fb3a5d94eec

--- extract_utils/extract_pixel.py
+++ extract_utils/extract_pixel.py
@@ -5,30 +5,28 @@
 
 from __future__ import annotations
 
-import shutil
-from os import path
-
 from extract_utils.extract import ExtractCtx, extract_zip
 from extract_utils.tools import fbpacktool_path
 from extract_utils.utils import run_cmd
 
-def copy_pixel_firmware(
+pixel_firmware_regex = r'(bootloader|radio)-.+\.img'
+pixel_factory_image_regex = r'image-.+\.zip'
+
+
+def extract_pixel_factory_image(
     ctx: ExtractCtx,
     file_path: str,
     work_dir: str,
     *args,
     **kwargs,
 ):
-    file_name = path.basename(file_path)
-    file_root, ext = path.splitext(file_name)
-
-    # Remove anything after (and including) the first dash
-    simple_file_root = file_root.split('-', 1)[0]
-
-    output_file_name = f'{simple_file_root}{ext}'
-    output_file_path = path.join(work_dir, output_file_name)
-
-    shutil.copy(file_path, output_file_path)
+    extract_zip(
+        file_path,
+        ctx,
+        [],
+        [],
+        work_dir,
+    )
     return file_path
 
 
